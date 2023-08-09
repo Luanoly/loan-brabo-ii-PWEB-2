d0ae87033c9139e42bef698bbffd470c82c5f4e6
Luanoly <los8@aluno.ifal.edu.br>
2023-08-09 15:55:33 -0300

    ajuste

--- App.js
+++ App.js
@@ -2,18 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
-
-// só pode ter 1 export
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Espaço de trabalho!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,

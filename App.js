92e49d7cd23fff030b30e450de74729fb9de3e8f
Luanoly <los8@aluno.ifal.edu.br>
2023-08-09 15:49:26 -0300

    adição de botão

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+// só pode ter 1 export
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Espaço de trabalho!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +22,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App1 = () => {
+  const [cliques, setCliques] = useState(0);
+
+  return(
+    <View style={styles.container}>
+      <Button title='OK' onPress={() => {
+        setCliques(cliques + 1);
+      }}/>
+      <Text>Quantidade de cliques: {cliques}</Text>
+    </View>
+  )
+}
+
+export default App1;

17be03b0457fc294156963a2d323ff3573c9e0af
Luanoly <los8@aluno.ifal.edu.br>
2023-08-09 16:18:51 -0300

    instituto e nome dos campis

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+
+const Campus = (props) => {
+  return(
+    <Text>O campus {props.nome} foi fundado em {props.anoFundacao}.</Text>
+  );
+}
+
+const InstitutoFederal = (props) => {
+  return(
+    <Text>{props.sigla} - {props.uf}</Text>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -8,20 +19,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
 
-const App1 = () => {
-  const [cliques, setCliques] = useState(0);
-
+const App = () => {
   return(
-    <View style={styles.container}>
-      <Button title='OK' onPress={() => {
-        setCliques(cliques + 1);
-      }}/>
-      <Text>Quantidade de cliques: {cliques}</Text>
+    <View style={styles.container} >
+      <InstitutoFederal sigla="IFAL" uf="AL" />
+      <Campus nome="Maceió" anoFundacao={1909} />
+      <Campus nome="Rio Largo" anoFundacao={2014} />
+      <Campus nome="Satuba" anoFundacao={1905} />
+      <InstitutoFederal sigla="IFPE" uf="PE" />
+      <Campus nome="Palmares" anoFundacao={2014} />
+      <Campus nome="Garanhuns" anoFundacao={2010} />
     </View>
-  )
+  );
 }
 
-export default App1;
+export default App;
